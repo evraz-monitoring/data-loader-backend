19276692c3e7fb146b1f081de09ecf09105d7844
Arthur Khe <artur4ik.khe@gmail.com>
2023-02-18 10:07:25 +0000

    Revert "Merge branch 'feature/add_data_to_queue' into 'main'"

--- exhauster/data_loader/models.py
+++ exhauster/data_loader/models.py
@@ -1,25 +1,3 @@
 from django.db import models
-# from timescale.fields import TimescaleDateTimeField
+
 # Create your models here.
-
-
-class SinterMachine(models.Model):
-    id = models.IntegerField(primary_key=True, null=False)
-
-
-class Metric(models.Model):
-    name = models.CharField(max_length=256)
-    measure = models.CharField(max_length=10, default="")
-
-
-class Exhauster(models.Model):
-    serviceability = models.BooleanField(default=True)
-    last_replacement_date = models.DateField()
-    sinter_machine = models.ForeignKey(SinterMachine, on_delete=models.CASCADE)
-
-
-class SystemIndicator(models.Model):
-    measurement_time = models.DateTimeField()
-    value = models.FloatField()
-    exhauster = models.ForeignKey(Exhauster, on_delete=models.CASCADE)
-    metric = models.ForeignKey(Metric, on_delete=models.CASCADE)
